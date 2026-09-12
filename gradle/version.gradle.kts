00cf4ded799a4877c6e5160e979c7b595d2e7948
RoundSalmon4 <209016228+RoundSalmon4@users.noreply.github.com>
2026-09-12 16:04:23 -0500

    Update version.gradle.kts

--- gradle/version.gradle.kts
+++ gradle/version.gradle.kts
@@ -1,4 +1,4 @@
 extra.set("appVersionMajor", 0)
 extra.set("appVersionMinor", 9)
-extra.set("appVersionPatch", 2)
-extra.set("appVersionCode", 16)
+extra.set("appVersionPatch", 3)
+extra.set("appVersionCode", 17)
